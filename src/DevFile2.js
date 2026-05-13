4dae35037b7c4d8b4339fcf53955d16bc8c130ce
vishwa <vishwa80123@gmail.com>
2026-05-14 00:30:12 +0530

    dev2_brnach3_commit2

--- src/DevFile2.js
+++ src/DevFile2.js
@@ -5,4 +5,9 @@ console.log("added by dev1")
 console.log("added by dev1")
 console.log("added by dev1")
 
+//for relase
+console.log("added by dev2")
+console.log("added by dev2")
+console.log("added by dev2")
+
 
